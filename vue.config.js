4307b73c92bbf3e97a8ca0d2c87ddf53f5c851fe
Florian Dehn <flo@katzefudder.de>
2023-11-21 12:15:53 +0100

    updated vue config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? "/FlickrGallery/"
-    : "/"
-})
+module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? "/FlickrGallery/" : "/",
+};
