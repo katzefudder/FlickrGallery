8da0da52630977e4e029455cacc160f531097dfa
Florian Dehn <flo@katzefudder.de>
2023-11-21 12:00:22 +0100

    add vue config for production

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/FlickrGallery/"
+  publicPath: process.env.NODE_ENV === 'production'
+    ? "/FlickrGallery/"
+    : "/"
 })
