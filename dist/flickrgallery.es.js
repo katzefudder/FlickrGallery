029e4e0b88fbf847c4b075cc17e0f2660dc0baf9
Florian Dehn <flo@katzefudder.de>
2024-03-02 11:35:19 +0100

    dist added

---
+++ dist/flickrgallery.es.js
@@ -0,0 +1,10 @@
+import { defineAsyncComponent as n } from "vue";
+const e = (o) => {
+  o.component(
+    "FlickrGallery",
+    n(() => import("./FlickrGallery-_vhFGJV8.js"))
+  );
+};
+export {
+  e as FlickrGalleryPlugin
+};
