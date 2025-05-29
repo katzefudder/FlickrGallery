bd35b673ef20170e28a98dbcbd212da4b9bf1c99
Florian Dehn <flo@katzefudder.de>
2025-05-29 11:32:03 +0200

    bugfix: Making sure Pinia is registered before the component

--- src/FlickrGallery.js
+++ src/FlickrGallery.js
@@ -1,5 +1,7 @@
 import {defineAsyncComponent} from "vue";
-export const FlickrGalleryPlugin = (app) => {
+export const FlickrGalleryPlugin = (app, pinia) => {
+    // Make sure Pinia is registered before the component
+    app.use(pinia);
     app.component(
         "FlickrGallery",
         defineAsyncComponent(() => import("./components/FlickrGallery.vue"))
